67d1450ac85b0d7e753668f47a6d2c292972813b
ViniTex231 <126170717+ViniTex231@users.noreply.github.com>
2023-05-08 11:23:55 -0300

    Add files via upload

---
+++ forca.js
@@ -0,0 +1,34 @@
+const palavras = [
+    palavra001={
+        nome:"IRLANDA",
+        categoria:"PAISES",
+    },
+    palavra002={
+        nome:"BRASIL",
+        categoria:"PAISES",
+    },
+    palavra003={
+        nome:"ARGENTINA",
+        categoria:"PAISES",
+    },
+    palavra004={
+        nome:"PIZZA",
+        categoria:"COMIDA",
+    },
+    palavra005={
+        nome:"HAMBURGUER",
+        categoria:"COMIDA",
+    },
+    palavra006={
+        nome:"COMPUTADOR",
+        categoria:"OBJETOS",
+    },
+    palavra007={
+        nome:"BANANA",
+        categoria:"FRUTA",
+    },
+    palavra008={
+        nome:"ABACATE",
+        categoria:"FRUTA",
+    },
+]
